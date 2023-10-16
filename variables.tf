d81e2d686a1b68d04daea3943223c7a298f00fd6
danishCloud <148103171+danishCloud@users.noreply.github.com>
2023-10-16 16:55:17 +0100

    Terraform Script to Deploy CDP on AWS

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "instance_cm" {
+  description = "cm"
+  type        = string
+  default     = "WS_CM_DB"
+}
+variable "instance_nn" {
+  description = "nn"
+  type        = string
+  default     = "NN"
+}
+variable "instance_snn" {
+  description = "snn"
+  type        = string
+  default     = "SNN"
+}
+variable "instance_gateway" {
+  description = "gateway"
+  type        = string
+  default     = "GATEWAY"
+}
+variable "instance_dn1" {
+  description = "dn1"
+  type        = string
+  default     = "DN1"
+}
+variable "instance_dn2" {
+  description = "dn2"
+  type        = string
+  default     = "DN2"
+}
+variable "instance_dn3" {
+  description = "dn3"
+  type        = string
+  default     = "DN3"
+}
